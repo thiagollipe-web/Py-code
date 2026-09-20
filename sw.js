db3087e8288137c6da600f2f8b44b6b33983960a
thiagollipe-web <thiagollipe@gmail.com>
2026-09-20 15:58:06 -0300

    Atualizar cache da engine do Py-Code

--- sw.js
+++ sw.js
@@ -1,36 +1,20 @@
-const CACHE="py-code-v3";
-const SHELL=["./","./index.html","./manifest.webmanifest","./assets/py-code-banner.svg"];
+const CACHE="py-code-v4";
+const SHELL=["./","./index.html","./manifest.webmanifest","./pycode.py","./assets/py-code-banner.svg"];
 
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));
 });
-
 self.addEventListener("activate",event=>{
-  event.waitUntil(
-    caches.keys().then(keys=>Promise.all(
-      keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
-    )).then(()=>self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
 });
-
 self.addEventListener("fetch",event=>{
-  const req=event.request;
-  const url=new URL(req.url);
-
-  if(url.origin!==location.origin) return;
-
+  const req=event.request,url=new URL(req.url);
+  if(url.origin!==location.origin)return;
   if(req.mode==="navigate"){
-    event.respondWith(
-      fetch(req).then(res=>{
-        const copy=res.clone();
-        caches.open(CACHE).then(c=>c.put("./index.html",copy));
-        return res;
-      }).catch(()=>caches.match("./index.html"))
-    );
+    event.respondWith(fetch(req).then(res=>{
+      const copy=res.clone();caches.open(CACHE).then(c=>c.put("./index.html",copy));return res;
+    }).catch(()=>caches.match("./index.html")));
     return;
   }
-
-  event.respondWith(
-    caches.match(req).then(cached=>cached||fetch(req))
-  );
+  event.respondWith(caches.match(req).then(cached=>cached||fetch(req)));
 });
